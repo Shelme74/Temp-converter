a9fd57f3e2fefb6752d162f46570ebf659caf438
Shelme74 <63882440+Shelme74@users.noreply.github.com>
2021-08-16 18:29:00 +0200

    Updated error suppressions

--- Temp converter/GlobalSuppressions.cs
+++ Temp converter/GlobalSuppressions.cs
@@ -24,4 +24,3 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>", Scope = "member", Target = "~M:Program.Program.Main")]
